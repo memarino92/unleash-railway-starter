43e293ff6cfc10bd250275ba5e518ab472a5c829
memarino92 <memarino92@gmail.com>
2021-12-13 17:47:57 -0500

    added server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const unleash = require('unleash-server');
+
+unleash
+  .start({
+    db: {
+      ssl: true,
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      database: process.env.PGDATABASE,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+    },
+    server: {
+      port: process.env.PORT,
+    },
+  })
+  .then((unleash) => {
+    console.log(
+      `Unleash started on http://localhost:${unleash.app.get('port')}`
+    );
+  });
